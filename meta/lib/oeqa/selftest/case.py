c1264911f73e9d7626c84b01b09337b25bbf32dd
Richard Purdie <richard.purdie@linuxfoundation.org>
2019-10-27 10:59:03 +0000

    selftest/signing: Use seperate layer test dir?
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/selftest/case.py
+++ meta/lib/oeqa/selftest/case.py
@@ -163,6 +163,7 @@ to ensure accurate results.")
             for command in self._extra_tear_down_commands:
                 result = runCmd(command, ignore_status=True)
                 if not result.status ==  0:
+                    self.logger.warning(result.output)
                     failed_extra_commands.append(command)
             if failed_extra_commands:
                 self.logger.warning("tearDown commands have failed: %s" % ', '.join(map(str, failed_extra_commands)))
